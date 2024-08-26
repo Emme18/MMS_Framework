cfaf32afeabba819650a53770516ac27407013d1
Emme18 <emelie.rademacher@stud.hs-hannover.de>
2024-08-26 16:37:45 +0200

    Transition Anfangsbutton

--- src/components/button.svelte
+++ src/components/button.svelte
@@ -1,3 +1,15 @@
-<button style="background-color: #004AAD; 
-font-family: Carter One, sans-serif; 
-font-size: 30px;" class="rounded p-4 text-white">Los Geht's</button>
+<button class="button rounded p-4 text-white">Los Geht's</button>
+
+<style>
+    .button {
+      background-color: #004AAD;
+      font-family: Carter One, sans-serif;
+      font-size: 30px;
+      transition: transform 0.3s ease-in-out;
+      transform: scale(1);
+    }
+  
+    .button:hover {
+      transform: scale(1.2);
+    }
+  </style>
