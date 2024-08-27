c34fbe50f28ca1ce352d0c46ef32877088401a8a
Emme18 <emelie.rademacher@stud.hs-hannover.de>
2024-08-27 21:53:08 +0200

    eisberg in position, aber größe nicht passend

--- src/routes/Spiel_starten/+page.svelte
+++ src/routes/Spiel_starten/+page.svelte
@@ -78,6 +78,30 @@ onMount(() => {
 
   moveAchteck();
  });
+
+ onMount(() => {
+  const iceberg = document.querySelector('.iceberg');
+  iceberg.style.position = 'absolute';
+  iceberg.style.top = '-240px'; // Höhe anpassen
+  iceberg.style.zIndex = '8';
+  const bahn2 = document.querySelector('.bahn2');
+  const screenWidth = window.innerWidth;
+  const screenHeight = window.innerHeight;
+
+  let icebergX = screenWidth;
+  let icebergSpeed = 2;
+
+  function moveIceberg() {
+    icebergX -= icebergSpeed;
+    if (icebergX < -screenWidth) {
+      icebergX = screenWidth;
+    }
+    iceberg.style.left = `${icebergX}px`;
+    requestAnimationFrame(moveIceberg);
+  }
+
+  moveIceberg();
+});
 </script>
 
 
@@ -127,7 +151,11 @@ onMount(() => {
         <div class="achteck-rechteck"></div>
       </div>
     </div>
-    <div class="bahn2" style="top: 45px;"></div>
+    <div class="bahn2" style="top: 45px;">
+      <div class="iceberg">
+        <span></span>
+      </div>
+    </div>
     <div class="bahn3" style="top: -100px;">
       <div class="orca" style="position: absolute; left: 0px;">
       <div id="orca">     
@@ -161,6 +189,181 @@ onMount(() => {
 </div> 
 
 <style>
+.iceberg {
+position: relative;
+z-index: 0;
+width: 0;
+height: 0;
+border-left: 185px solid transparent;
+border-right: 185px solid transparent;
+border-bottom: 185px solid #004AAD;
+animation: float 4s ease-in-out infinite;
+}
+
+.iceberg::before {
+content: '';
+display: block;
+position: absolute;
+top: 185px;
+left: -185px;
+width: 0;
+height: 0;
+border-left: 95px solid transparent;
+border-right: 95px solid transparent;
+border-top: 95px solid lightblue;
+}
+
+.iceberg::after {
+content: '';
+display: block;
+position: absolute;
+top: 185px;
+width: 0;
+height: 0;
+border-left: 95px solid transparent;
+border-right: 95px solid transparent;
+border-top: 95px solid lightblue;
+}
+
+.iceberg span {
+position: absolute;
+z-index: 1;
+left: 0;
+top: 0;
+margin-left: -100px;
+display: block;
+width: 0;
+height: 0;
+border-left: 185px solid transparent;
+border-right: 185px solid transparent;
+border-bottom: 185px solid darkblue;
+animation: float2 4s ease-in-out infinite;
+}
+
+.iceberg span::before {
+content: '';
+display: block;
+position: absolute;
+top: 185px;
+margin-left: -5px;
+width: 0;
+height: 0;
+border-left: 95px solid transparent;
+border-right: 95px solid transparent;
+border-top: 95px solid #89cff3;
+z-index: -1;
+}
+
+.iceberg span::after {
+content: '';
+display: block;
+position: absolute;
+top: 185px;
+margin-left: -185px;
+z-index: -1;
+width: 0;
+height: 0;
+border-left: 95px solid transparent;
+border-right: 95px solid transparent;
+border-top: 95px solid #89cff3;
+}
+
+@-moz-keyframes float {
+0% {
+  transform: translateY(15px);
+}
+50% {
+  transform: translateY(0);
+}
+100% {
+  transform: translateY(15px);
+}
+}
+
+@-webkit-keyframes float {
+0% {
+  transform: translateY(15px);
+}
+50% {
+  transform: translateY(0);
+}
+100% {
+  transform: translateY(15px);
+}
+}
+
+@-o-keyframes float {
+0% {
+  transform: translateY(15px);
+}
+50% {
+  transform: translateY(0);
+}
+100% {
+  transform: translateY(15px);
+}
+}
+
+@keyframes float {
+0% {
+  transform: translateY(15px);
+}
+50% {
+  transform: translateY(0);
+}
+100% {
+  transform: translateY(15px);
+}
+}
+
+@-moz-keyframes float2 {
+0% {
+  transform: translateY(5px);
+}
+50% {
+  transform: translateY(-5px);
+}
+100% {
+  transform: translateY(5px);
+}
+}
+
+@-webkit-keyframes float2 {
+0% {
+  transform: translateY(5px);
+}
+50% {
+  transform: translateY(-5px);
+}
+100% {
+  transform: translateY(5px);
+}
+}
+
+@-o-keyframes float2 {
+0% {
+  transform: translateY(5px);
+}
+50% {
+  transform: translateY(-5px);
+}
+100% {
+  transform: translateY(5px);
+}
+}
+
+@keyframes float2 {
+0% {
+  transform: translateY(5px);
+}
+50% {
+  transform: translateY(-5px);
+}
+100% {
+  transform: translateY(5px);
+}
+}
+
 .penguin-bottom .right-feet, .penguin-bottom .left-feet {
   animation: walk 2s infinite;
 }
--- src/routes/Versuchsumgebung/+page.svelte
+++ src/routes/Versuchsumgebung/+page.svelte
@@ -1,49 +1,169 @@
-<!-- Leer bis notwendig -->
+
+	<div class="iceberg">
+		<span></span>
+	</div>
 
 <style>
-.cloud1,  
-.cloud2,  
-.cloud3,  
-.cloud4,  
-.cloud5,  
-.cloud6 {  
 
-  position: relative; /* relative führt zu pinguin und bahnen unten platziert und wolken fliegen über den ganzen bildschirm, fixed wolken nur obere 50% und pinguin und bahnen mittig im Bild */
-  width: 100%;
-  height: 100%; 
-
-  -webkit-border-radius: 200px;    
-  -moz-border-radius: 200px;    
-  border-radius: 200px; 
-}    
+.iceberg {
+  position: relative;
+  z-index: 0;
+  width: 0;
+  height: 0;
+  border-left: 185px solid transparent;
+  border-right: 185px solid transparent;
+  border-bottom: 185px solid #004AAD;
+  animation: float 4s ease-in-out infinite;
+}
+.iceberg::before {
+  content: '';
+  display: block;
+  position: absolute;
+  top: 185px;
+  left: -185px;
+  width: 0;
+  height: 0;
+  border-left: 95px solid transparent;
+  border-right: 95px solid transparent;
+  border-top: 95px solid lightblue;
+}
+.iceberg::after {
+  content: '';
+  display: block;
+  position: absolute;
+  top: 185px;
+  width: 0;
+  height: 0;
+  border-left: 95px solid transparent;
+  border-right: 95px solid transparent;
+  border-top: 95px solid lightblue;
+}
+.iceberg span {
+  position: absolute;
+  z-index: 1;
+  left: 0;
+  top: 0;
+  margin-left: -100px;
+  display: block;
+  width: 0;
+  height: 0;
+  border-left: 185px solid transparent;
+  border-right: 185px solid transparent;
+  border-bottom: 185px solid darkblue;
+  animation: float2 4s ease-in-out infinite;
+}
+.iceberg span::before {
+  content: '';
+  display: block;
+  position: absolute;
+  top: 185px;
+  margin-left: -5px;
+  width: 0;
+  height: 0;
+  border-left: 95px solid transparent;
+  border-right: 95px solid transparent;
+  border-top: 95px solid #89cff3;
+  z-index: -1;
+}
+.iceberg span::after {
+  content: '';
+  display: block;
+  position: absolute;
+  top: 185px;
+  margin-left: -185px;
+  z-index: -1;
+  width: 0;
+  height: 0;
+  border-left: 95px solid transparent;
+  border-right: 95px solid transparent;
+  border-top: 95px solid #89cff3;
+}
+@-moz-keyframes float {
+  0% {
+    transform: translateY(15px);
+  }
+  50% {
+    transform: translateY(0);
+  }
+  100% {
+    transform: translateY(15px);
+  }
+}
+@-webkit-keyframes float {
+  0% {
+    transform: translateY(15px);
+  }
+  50% {
+    transform: translateY(0);
+  }
+  100% {
+    transform: translateY(15px);
+  }
+}
+@-o-keyframes float {
+  0% {
+    transform: translateY(15px);
+  }
+  50% {
+    transform: translateY(0);
+  }
+  100% {
+    transform: translateY(15px);
+  }
+}
+@keyframes float {
+  0% {
+    transform: translateY(15px);
+  }
+  50% {
+    transform: translateY(0);
+  }
+  100% {
+    transform: translateY(15px);
+  }
+}
+@-moz-keyframes float2 {
+  0% {
+    transform: translateY(5px);
+  }
+  50% {
+    transform: translateY(-5px);
+  }
+  100% {
+    transform: translateY(5px);
+  }
+}
+@-webkit-keyframes float2 {
+  0% {
+    transform: translateY(5px);
+  }
+  50% {
+    transform: translateY(-5px);
+  }
+  100% {
+    transform: translateY(5px);
+  }
+}
+@-o-keyframes float2 {
+  0% {
+    transform: translateY(5px);
+  }
+  50% {
+    transform: translateY(-5px);
+  }
+  100% {
+    transform: translateY(5px);
+  }
+}
+@keyframes float2 {
+  0% {
+    transform: translateY(5px);
+  }
+  50% {
+    transform: translateY(-5px);
+  }
+  100% {
+    transform: translateY(5px);
+  }
+}
 </style>
-
-<script>
-onMount(() => {
-  const penguin = document.querySelector('.penguin3');
-  const orca = document.querySelector('.orca');
- 
-  function stopAnimation() {
-  orca.style.animationPlayState = 'paused';
-  }
- 
-  function checkCollision() {
-  const penguinRect = penguin.getBoundingClientRect();
-  const orcaRect = orca.getBoundingClientRect();
-  if (penguinRect.left < orcaRect.right &&
-   penguinRect.right > orcaRect.left &&
-   penguinRect.top < orcaRect.bottom &&
-   penguinRect.bottom > orcaRect.top) {
-  stopAnimation();
-  }
-  }
- 
-  function moveOrca() {
-  // Animation-Code hier
-  checkCollision();
-  requestAnimationFrame(moveOrca);
-  }
- 
-  moveOrca();
- });
- </script>
